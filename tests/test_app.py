bcac1f08645b7d355940060e098c44c74aa2cf61
Shlok Shetty <shlokbs14@gmail.com>
2026-09-26 17:36:57 +0530

    chore: add linting and pytest configuration

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,4 @@
-import os
-import sys
-
 import pytest
-
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..")
-    )
-)
 
 from app import app, equipment
 
